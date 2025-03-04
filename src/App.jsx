a80f78e6958bbcd2c2533b5f3f387525a3c3d910
Rhosen23 <1453477168@qq.com>
2025-03-05 04:19:25 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ToDoList from './Components/ToDoList'
+
+function App() {
+
+  return (
+    <ToDoList/>
+  )
+}
+
+export default App
---
+++ src/Components/ToDoList.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import './ToDoList.css'
+
+export default function ToDoList() {
+
+  const [tasks, setTasks] = useState([
+    'Eat Breakfast', 'Take shower', 'Sleep'
+  ])
+  const [newTasks, setNewTasks] = useState('')
+
+  function handleInput(event) {
+    setNewTasks(event.target.value)
+  }
+
+  function addTask() {
+    if (newTasks.trim !== '') {
+      setTasks([...tasks, newTasks])
+      setNewTasks('')
+    }
+  }
+
+  function deleteTask(index) {
+    setTasks(tasks.filter((_, i) => i !== index))
+  }
+
+  function upTask(index) {
+    if (index > 0) {
+      const newTasksArray = [...tasks];
+      [newTasksArray[index], newTasksArray[index - 1]] = [newTasksArray[index - 1], newTasksArray[index]];
+      setTasks(newTasksArray);
+    }
+  }
+
+  function downTask(index) {
+    if (index != tasks.length - 1) {
+      const newTasksArray = [...tasks];
+      [newTasksArray[index], newTasksArray[index + 1]] = [newTasksArray[index + 1], newTasksArray[index]];
+      setTasks(newTasksArray);
+    }
+  }
+
+
+  return (
+    <div className='to-do-list'>
+      <h1>ToDoList</h1>
+
+      <div>
+        <input type="text"
+          placeholder='Enter a task...'
+          value={newTasks}
+          onChange={handleInput} />
+
+        <button className='add-button'
+          onClick={addTask}>
+          Add
+        </button>
+      </div>
+
+      <ol>
+        {tasks.map((task, index) =>
+          <li key={index}>
+            <span className='text'>{task}</span>
+            <button className='delete-button'
+              onClick={() => deleteTask(index)}>Delete
+            </button>
+            <button className='move-button'
+              onClick={() => upTask(index)}>Up
+            </button>
+            <button className='move-button'
+              onClick={() => downTask(index)}>Down
+            </button>
+          </li>
+        )}
+      </ol>
+
+    </div>
+  )
+}
